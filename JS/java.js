7c4ff4a2e83aab449b088c76acc1ffc18d548aa1
MiguelOutrac <j.miguelito.sc@gmail.com>
2024-11-28 20:38:04 -0300

    sétimo commit/ o site supremo, ainda falta algumas coisas

--- JS/java.js
+++ JS/java.js
@@ -1,14 +1,29 @@
 new Swiper('.card-wrapper', {
-    loop: true,
-  
-    
-    pagination: {
-      el: '.swiper-pagination',
+  loop: true,
+  spaceBetween: 30,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets:true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    0:{
+      slidesPerView:1
     },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+    768: {
+      slidesPerView:2
     },
-  });
+    1024: {
+      slidesPerView:3
+    },
+  }
+});
